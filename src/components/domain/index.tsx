13826a3be2422849d05e5b663efe947bc768355e
slerf <slerf@memes.ac>
2024-12-17 20:43:57 +0800

    Update index.tsx

--- src/components/domain/index.tsx
+++ src/components/domain/index.tsx
@@ -17,7 +17,7 @@ export const memesTitleSize =
 export const memesSubTitleSize =
   "text-2xl sm:text-3xl xl:text-4xl font-normal orbitron";
 export const memesTextSize =
-  " text-sm sm:text-sm xl:text-base font-normal opacity-50";
+  " text-sm sm:text-sm xl:text-base font-normal opacity-100";
 export const memesBntColor =
   "bg-gradient-to-b from-[#FFAC03] to-[#FFC10B] text-black";
 export const memesTextColor =
