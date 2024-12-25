a7b797d7d24a78f8441e9a6aca2f7c5440afd3d6
jay <3334354196@qq.com>
2024-12-25 15:39:54 +0800

    Fix left and right sliding

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Space } from '@/components/domain';
-import Header from './Header';
+import React from 'react'
+import { Space } from '@/components/domain'
+import Header from './Header'
 
 interface LayoutProps {
-  children: React.ReactNode;
-  data: any;
+  children: React.ReactNode
+  data: any
 }
 
 const Layout: React.FC<LayoutProps> = ({ children, data }) => {
@@ -12,11 +12,11 @@ const Layout: React.FC<LayoutProps> = ({ children, data }) => {
     <>
       <Space />
       <Header />
-      <main className="px-3 py-4 md:px-8 lg:px-16 w-full max-w-screen-xl overflow-y-hidden mx-auto z-10 pt-24 sm:pt-36">
+      <main className="px-3 py-4 md:px-8 lg:px-16 w-full max-w-screen-xl overflow-hidden mx-auto z-10 pt-24 sm:pt-36">
         {children}
       </main>
     </>
-  );
-};
+  )
+}
 
-export default Layout; 
+export default Layout
