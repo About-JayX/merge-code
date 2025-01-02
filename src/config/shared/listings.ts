e48e5e36e895a597dca7e11b13bf57993c21f2aa
s3042HJX <2038392181@qq.com>
2025-01-02 15:57:28 +0800

    更新图片及链接

--- src/config/shared/listings.ts
+++ src/config/shared/listings.ts
@@ -1,10 +1,4 @@
-import SpacePiSolPng from "@/assets/exchange/SpacePi-sol.png";
-import HibtSvg from "@/assets/exchange/hibt.svg";
 import BinanceSvg from "@/assets/exchange/binance.svg";
-import BitgetSvg from "@/assets/exchange/bitget.svg";
-import LbankSvg from "@/assets/exchange/lbank.svg";
-import MexcSvg from "@/assets/exchange/mexc.svg";
-import XtSvg from "@/assets/exchange/xt.svg";
 import PumpSvg from "@/assets/exchange/pump.svg";
 import DexscreenerPng from "@/assets/exchange/dexscreener.png";
 import CoingeckoPng from "@/assets/exchange/coingecko.png";
@@ -14,6 +8,30 @@ import GatePng from "@/assets/exchange/gate.png";
 import DextoolsPng from "@/assets/exchange/dextools.png";
 export default [
   {
+    image: PumpSvg,
+    url: "https://pump.fun/coin/8J6CexwfJ8CSzn2DgWhzQe1NHd2hK9DKX59FCNNMo2hu",
+  },
+  {
+    image: OkxPng,
+    url: "https://www.okx.com/web3/detail/501/8J6CexwfJ8CSzn2DgWhzQe1NHd2hK9DKX59FCNNMo2hu",
+  },
+  {
+    image: BinanceSvg,
+    url: "https://www.binance.com/en/price/mini-doge-com",
+  },
+  {
+    image: CoinmarketcapPng,
+    url: "https://coinmarketcap.com/currencies/mini-doge-com/",
+  },
+  {
+    image: CoinmarketcapPng,
+    url: "https://coinmarketcap.com/community/profile/MINIDOGE_X/",
+  },
+  {
+    image: CoingeckoPng,
+    url: "https://www.coingecko.com/en/coins/minidoge-2",
+  },
+  {
     image: DexscreenerPng,
     url: "https://dexscreener.com/solana/cfwwtfeyr4ai9tmpbte21jsewhum3pmytaluf9x2b5ej",
   },
@@ -22,31 +40,11 @@ export default [
     url: "https://www.dextools.io/app/en/solana/pair-explorer/CFWwTFEyr4ai9tMPBtE21JSewhum3PMyTALUF9x2B5eJ",
   },
   {
-    image: CoinmarketcapPng,
-    url: "https://coinmarketcap.com/dexscan/solana/GEV2dBkXx4X5Xy8FFX7eR1TNhWyj6m1bp66tyTUGicBB/",
-  },
-  {
-    image: CoingeckoPng,
-    url: "https://www.coingecko.com/en/coins/minidoge-2",
-  },
-  {
-    image: OkxPng,
-    url: "https://www.okx.com/web3/detail/501/8J6CexwfJ8CSzn2DgWhzQe1NHd2hK9DKX59FCNNMo2hu",
-  },
-  {
     image: GatePng,
     url: "https://www.gate.io/pilot/solana/minidoge-minidog",
   },
-  {
-    image: PumpSvg,
-    url: "https://pump.fun/coin/8J6CexwfJ8CSzn2DgWhzQe1NHd2hK9DKX59FCNNMo2hu",
-  },
   // {
   //   image: HibtSvg,
-  //   url: "",
-  // },
-  // {
-  //   image: BinanceSvg,
   //   url: "",
   // },
   // {
